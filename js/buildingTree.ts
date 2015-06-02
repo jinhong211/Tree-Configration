d00e37e693ce310eac12531dde33a02c8f3f7cf9
Benjamin Lissillour <lissillour.benjamin@etu.unice.fr>
2015-06-02 11:00:48 +0200

    simple commit

---
+++ js/buildingTree.ts
@@ -0,0 +1,14 @@
+///<reference path="./treeNode.ts"/>
+
+/**
+ * Created by ben on 01/06/2015.
+ */
+
+class buildingTree {
+    private racine: TreeNode;
+    private selected: TreeNode[];
+    private available: TreeNode[];
+
+    render() : void {
+    }
+}
