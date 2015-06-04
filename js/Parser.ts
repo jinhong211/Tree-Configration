79053d2f6e2cc3bafc36ebab81de0a7812603293
Benjamin Lissillour <lissillour.benjamin@etu.unice.fr>
2015-06-04 16:24:21 +0200

    Check de l'erreur de l'envoi d'un arbre vide

--- js/Parser.ts
+++ js/Parser.ts
@@ -91,6 +91,8 @@ class Parser {
         var xml = document.createElement("root");
         var bloc;
 
+        console.log("I'm here :)");
+
         if (noeudCourant instanceof ActionTreeNode) {
             bloc = document.createElement("task");
             var type = document.createElement("type");
@@ -114,7 +116,9 @@ class Parser {
                 bloc.appendChild(childrenNode);
             }
         }
-        xml.appendChild(bloc);
+        if (bloc) {
+            xml.appendChild(bloc);
+        }
 
         if (init) {
             var shell = document.createElement("shell");
