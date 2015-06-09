f80efa70689413672e18658754d84b7612ffc1cf
Anais Marongiu <ana.marongiu@gmail.com>
2015-06-09 17:18:55 +0200

    mise en place des tests PQJN-68

--- test/firstTest.ts
+++ test/firstTest.ts
@@ -1,6 +1,9 @@
 ///<reference path="..\typings\node\node.d.ts"/>
 ///<reference path="..\typings\mocha\mocha.d.ts"/>
 ///<reference path="..\typings\chai\chai.d.ts"/>
+
+///<reference path="..\js\Parser.ts"/>
+///<reference path="..\js\Communication.ts"/>
 /**
  * Created by A on 09/06/2015.
  */
@@ -13,5 +16,11 @@ describe("My first unit test in TS", () => {
         chai.expect(function() {
             throw new Error("derp")
         }).to.throw(Error);
+
+
+    })
+
+    it("should throw exception", () => {
+        chai.expect(new Parser().parseBlackboard3(new Communication("http://46.105.18.34:8080").httpGetMOCK3())).to.be.equal("ee");
     })
 });
