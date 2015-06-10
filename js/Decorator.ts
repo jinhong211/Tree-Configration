e63cc534221fffb168cbac409519349ca42693a4
Quentin cornevin <qcornevin@gmail.com>
2015-06-10 10:31:17 +0200

    PGJN-69 : Mise en place correct des decorator avec leurs params

--- js/Decorator.ts
+++ js/Decorator.ts
@@ -5,9 +5,28 @@
  */
 class Decorator extends TreeNode{
 
-    //private settableNumberParameter : Array<number>;
+    private settableParameter : Array<number | string>;
 
-   // public constructor(n: string, nameDisplayed = "", description = "", )
+    /**
+     * Constructor of the decorator with a name and an array of string or number containing
+     * all the parameter of the decorator
+     * @param n
+     * @param settableParams
+     * @param nameDisplayed
+     * @param description
+     */
+    public constructor(n: string, settableParams : Array<number | string> ,nameDisplayed = "", description = "" ) {
+        super(n,nameDisplayed,description);
+        this.settableParameter = settableParams;
+    }
 
+    /**
+     * This method is a getter for the settable parameter.
+     * @returns {Array<number|string>}
+     */
+    public getParams(): Array<number | string> {
+        return this.settableParameter;
+    }
 
+    // TODO le set !
 }
