f840bc419b6246f6744aaee898a1e79e0bd0892c
Anais Marongiu <ana.marongiu@gmail.com>
2015-06-12 09:33:49 +0200

    PQJN-75 resolution bug envoi arbre vide

--- js/Parser.ts
+++ js/Parser.ts
@@ -219,8 +219,9 @@ class Parser {
 
 
     parseXml3Decorator(currentNode : TreeNode) : HTMLElement {
+        var decs = null;
         // DECORATORS
-        if (currentNode.getDecorators() != null) {
+        if (currentNode != null && currentNode.getDecorators() != null) {
             console.log("decorators!");
             var decs = document.createElement("decorators");
             for (var i = 0; i < currentNode.getDecorators().length; i++) {
@@ -312,7 +313,9 @@ class Parser {
 
         // DECORATORS
         var decs = this.parseXml3Decorator(currentNode);
-        bloc.appendChild(decs);
+        if(decs != null) {
+            bloc.appendChild(decs);
+        }
 
         // FINI ?
         if (bloc) {
